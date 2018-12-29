7ed1541e28c4fab6a9a9341068fdb2b4369e0016
Sudi Gopinath <1133998+sugop@users.noreply.github.com>
2018-12-28 21:55:07 -0500

    day 1.2 solved

--- 2018/day1/day1.py
+++ 2018/day1/day1.py
@@ -1,15 +1,25 @@
-def read_integers(filename):
+def get_freq_changes(filename):
     with open(filename,'r') as f:
         return list(map(int, f))
 
-def read_freq(filename):
-        with open(filename,'r') as f:
-            return map(int,f) 
-
 def main():
-    freqs = read_integers('input.txt')
+    #changes = [-6, +3, +8, +5, -6]
+    #changes = [+1, -1]
+    changes = get_freq_changes('input1.txt')
 
     #part 1: Calc sum of frequencies
-    print(sum(freqs))
+    print("Sum = ", sum(changes))
+
+    #part 2: find first duplicate instance of calculated frequency
+    freq = 0
+    freq_history = {0: True}
+    ctr = 0
+    while True:
+        freq += changes[ctr%len(changes)]
+        if freq in freq_history:
+            print("First freq reached twice = ", freq)
+            break
+        freq_history[freq] = True
+        ctr += 1
 
 main()
