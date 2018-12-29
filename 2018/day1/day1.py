3505921881e3a5643ecfa4950c794ba52414c56a
Sudi Gopinath <1133998+sugop@users.noreply.github.com>
2018-12-28 20:35:04 -0500

    day 1.1 solved

--- 2018/day1/day1.py
+++ 2018/day1/day1.py
@@ -1,7 +1,15 @@
-try:
-    input_file = open('input.txt','r')
-    freq_list = input_file.readlines()
-    print(freq_list)
-    input_file.close()
-except IOError as ioEx:
-    print(ioEx)
+def read_integers(filename):
+    with open(filename,'r') as f:
+        return list(map(int, f))
+
+def read_freq(filename):
+        with open(filename,'r') as f:
+            return map(int,f) 
+
+def main():
+    freqs = read_integers('input.txt')
+
+    #part 1: Calc sum of frequencies
+    print(sum(freqs))
+
+main()
