a43d31cb763f66f4a93e2d1f718bc009eb5f48d7
Sudarshan Gopinath <sudarshan.gopinath@grmemea30176.mynet>
2020-12-03 14:47:07 +0000

    aoc3.2: day 3 puzzle 2 completed + minor refactor

--- 2020/day03/day03.py
+++ 2020/day03/day03.py
@@ -12,7 +12,7 @@ def read_map_grid(filename: str) -> list:
         lines = [list(line.strip()) for line in textFile] #strip EOLs and convert to list of lists
         return lines
 
-def count_trees_part1(values: list, dx: int, dy: int) -> int:
+def count_trees(values: list, dx: int, dy: int) -> int:
     x, y, cnt, length, mod = 0, 0, 0, len(values) - dy, len(values[0])
     while y < length:
         x = (x + dx) % mod
@@ -21,8 +21,23 @@ def count_trees_part1(values: list, dx: int, dy: int) -> int:
             cnt += 1
     return cnt
 
+def solve_part1(area_map: list) -> int:
+    return count_trees(area_map, 3, 1)
+
+def solve_part2(area_map: list, sol_part1: int) -> int:
+    slopes = ((1, 1), (5, 1), (7, 1), (1, 2))
+    product = sol_part1
+
+    for slope in slopes:
+        product *= count_trees(area_map, slope[0], slope[1])
+    return product
+
 def main():
-    map_grid = read_map_grid('day03_input.txt')
-    print(count_trees_part1(map_grid, 3, 1))
+    area_map = read_map_grid('day03_input.txt')
+    sol_part1 = solve_part1(area_map)
+    sol_part2 = solve_part2(area_map, sol_part1)
+
+    print('Part 1: Count of Trees = ', sol_part1)
+    print('Part 2: Count of Trees = ', sol_part2)
 
 main()
